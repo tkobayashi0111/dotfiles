518e9bb1a2d145d1c756d54f009ec9e40050aec8
tkobayashi0111 <kobataku0629@gmail.com>
2022-07-14 09:25:19 +0900

    fix lsp definition

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -250,7 +250,7 @@ return packer.startup(function(use)
           local opts = { noremap = true, silent = true }
           buf_set_keymap('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
           -- buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-          buf_set_keymap("n", "gD", ":tab split<cr><cmd>lua vim.lsp.buf.definition()<CR>", opts)
+          buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
           -- buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
           buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
           -- lspsaga
